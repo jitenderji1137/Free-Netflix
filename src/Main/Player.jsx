dfe2c812044158291250d6f4506db850dfd2ea50
jitender <trademetrader1137@gmail.com>
2023-04-07 21:35:57 +0530

    free netflix

--- src/Main/Player.jsx
+++ src/Main/Player.jsx
@@ -38,15 +38,19 @@ export default function Player(){
         <>
         <Helmet>
         <title>{`Free Netflix - ${Title.split("_").join(" ")} ||  Watch Free Movies or WebSeries online or Download || Created BY TRADEmeTRADER as Jitender or Vijay`}</title>
-        <meta name='description' content={`Watch - ${Title.split("_").join(" ")} for free ||  Watch Free Movies or WebSeries online or Download || Created BY TRADEmeTRADER as Jitender or Vijay`} />
         <meta name="keywords" content="Free-NetFlix, netflix, freenetflix, vijay, jitender,jitenderji1137,vijayji1137,watch netflix free , download movies free , how to download movies free , watch netflix movies free , how to watch netflix movies for free , netflix clone , netflix free , how to download netflix movies for free , watch free movies , NETFLIX , FREE NETFLIX , how can i watch netflix for free , how to access netflix for free" />
-        <meta name="language" content="Hindi" />
-        <meta property="og:image" content={img} />
-        <meta property="og:url" content={shareUrl}/>
-        <meta property="og:type" content="movie" />
-        <meta name="country" content="India" />
-        <meta name="instagram:username" content="vijayji1137" />
-        <meta property="og:type" content="profile" />
+        <link href={shareUrl} rel='canonical'/>
+        <meta content={`Watch - ${Title.split("_").join(" ")} for free ||  Watch Free Movies or WebSeries online or Download || Created BY TRADEmeTRADER as Jitender or Vijay`}  name='description'/>
+        <link href={img} rel='image_src'/>
+        <meta content={`Free Netflix - ${Title.split("_").join(" ")} ||  Watch Free Movies or WebSeries online or Download || Created BY TRADEmeTRADER as Jitender or Vijay`} property='og:title'/>
+        <meta content={shareUrl} property='og:url'/>
+        <meta content='Free Netflix - Watch Free Movies or WebSeries online or Download' property='og:site_name'/>
+        <meta content={img} property='og:image'/>
+        <meta content={img} name='twitter:image'/>
+        <meta content='summary_large_image' name='twitter:card'/>
+        <meta content={`Free Netflix - ${Title.split("_").join(" ")} ||  Watch Free Movies or WebSeries online or Download || Created BY TRADEmeTRADER as Jitender or Vijay`} name='twitter:title'/>
+        <meta content={shareUrl} name='twitter:domain'/>
+        <meta content={`Watch - ${Title.split("_").join(" ")} for free ||  Watch Free Movies or WebSeries online or Download || Created BY TRADEmeTRADER as Jitender or Vijay`} name='twitter:description'/>
         </Helmet>
         {Plateform==="Doodstream"?
         <>
