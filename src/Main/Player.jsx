13e9209ad39c5bc1c61bbac1f1fca8a2fe0197df
jitender <trademetrader1137@gmail.com>
2023-04-07 13:07:22 +0530

    free netflix

--- src/Main/Player.jsx
+++ src/Main/Player.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { Helmet } from 'react-helmet-async';
 import "./Player.css";
 import "./Allinone.css"
-import { FacebookShareButton,FacebookIcon,OKShareCount,PinterestShareCount,RedditShareCount,} from "react-share";
+import { FacebookShareButton,FacebookIcon} from "react-share";
 export default function Player(){
     const { Title , Geans , Plateform , Id , page } = useParams();
     const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
