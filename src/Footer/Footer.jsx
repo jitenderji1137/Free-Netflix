e54fb655820201f204a8045c09911e9556838217
jitender <trademetrader1137@gmail.com>
2023-05-28 20:46:03 +0530

    Free-Netflix

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,12 +1,58 @@
+import { useEffect,useState } from "react";
 import "./Footer.css";
-import { Link } from "react-router-dom";
+import { Link , useNavigate } from "react-router-dom";
 import { MdHome , MdMovie } from "react-icons/md";
 import { FaSearch,FaYoutube } from "react-icons/fa";
 import { RiSpyFill } from "react-icons/ri";
 import {IoIosVideocam } from "react-icons/io";
 import { BsFillPersonFill,BsCCircle } from "react-icons/bs";
 import { AiOutlineLinkedin,AiOutlineInstagram,AiOutlineGithub } from "react-icons/ai";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import Swal from 'sweetalert2'
 export default function Footer(){
+    const navigate = useNavigate();
+    const [user, setIsAuthenticated] = useState(false);
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const firebaseConfig = {
+        apiKey: "AIzaSyBOwu1HGOc2LTTjalwwhwEkM16EdziUyEE",
+        authDomain: "free-netflix-7e3cf.firebaseapp.com",
+        projectId: "free-netflix-7e3cf"
+    };
+    firebase.initializeApp(firebaseConfig);
+    useEffect(() => {
+        // Add an authentication state observer
+        const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+            // User is signed in
+            setIsAuthenticated(true);
+          } else {
+            // User is signed out
+            setIsAuthenticated(false);
+          }
+        });
+    
+        // Clean up the observer when component unmounts
+        return () => {
+          unsubscribe();
+        };
+      }, []);
+      function navlogout() {
+        Swal.fire({
+          title: 'Are you sure?',
+          text: "Do you want to logout...",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, Logout'
+        }).then((result) => {
+          if (result.isConfirmed) {
+              firebase.auth().signOut();
+              navigate(`/`);
+          }
+        })
+  }
     return(
         <>
         <footer className="hero">
@@ -94,10 +140,16 @@ export default function Footer(){
             <RiSpyFill/>
             <span>Adult</span>    
             </button></Link>
-            <Link to="https://www.instagram.com/vijayji1137/" target="_blank" rel="noreferrer"><button className="tab-item">
+            {user ?
+            <div onClick={navlogout()} target="_blank" rel="noreferrer"><button className="tab-item">
             <BsFillPersonFill/>
-            <span>Contact</span>    
-            </button></Link>
+            <span>Logout</span>    
+            </button></div>:
+            <div onClick={firebase.auth().signInWithPopup(provider)} target="_blank" rel="noreferrer"><button className="tab-item">
+            <BsFillPersonFill/>
+            <span>Login</span>    
+            </button></div>
+            }
         </div>
         </>
     )
